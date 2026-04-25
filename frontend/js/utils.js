94033b1059f183c83f3add0a1e9127a51289d4d6
Manas <manasmaru83@gmail.com>
2026-04-25 17:15:48 +0530

    fix: update logout redirect

--- frontend/js/utils.js
+++ frontend/js/utils.js
@@ -1,12 +1,12 @@
 export function requireAuth() {//if user_id exists in localstorage return it else redirect to login.html
   const userId = localStorage.getItem("user_id");
   if (!userId) {
-    window.location.href = "login.html";
+    window.location.href = "/"; // redirect to index.html
   }
   return userId;
 }
 
 export function logout() {//remove user_id from localstorage and redirect to login.html
   localStorage.removeItem("user_id");
-  window.location.href = "login.html";
+  window.location.href = "/"; // redirect to index.html
 }
